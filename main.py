2ef1eb3a3b25605de44fd5d0ea7401834fcc7323
Nadia <rnadanadia@gmail.com>
2022-07-01 08:30:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ headers = {
 
 driver = webdriver.Safari()
 
+primary_keys = []
 property_types = []
 prices = []
 street_names = []
@@ -35,47 +36,70 @@ surface_plots = []
 building_conditions = []
 energy_classes = []
 
-def get_property(links):
+
+
+def get_property_and_primary_keys(links):
     property = links.split('/')[5]  
     property_types.append(property)
+    
+    primary_key_link = links.split('/')[-1]
+    primary_key = primary_key_link.split('?')[0] 
+    primary_keys.append(primary_key)
+    print("Primary key: ", primary_keys[-1])
     print("Property type: ", property_types[-1])
     return
 
-        
+
 def get_price(soup):
     class_price = soup.find('p', {'class': 'classified__price'})
-    every_price = class_price.find('span', {'class':'sr-only'}).text
-    price = (re.findall(r'[\d]+', every_price)[0])
-    prices.append(price)
-    print("Price: ", price)
-    return
+    if class_price is not None:
+        every_price = class_price.find('span', {'class':'sr-only'}).text
+        if every_price is not None:
+            price = (re.findall(r'[\d]+', every_price)[0])
+            prices.append(price)
+            print("Price: ", price)
+        else:
+            prices.append(None)
+    else:
+        prices.append(None)
 
 
 def get_address(soup):
     address_rows = soup.find('div', {'class':'classified__information--address'})
-    address_row = address_rows.find_all('span', {'class': 'classified__information--address-row'})
-    if len(address_row)==2:
-        street_name = address_row[0].text.strip()
-        street_names.append(street_name)
-        zipcode_and_locality = address_row[1].text.strip().split('—')
-        zipcode = zipcode_and_locality[0].strip()
-        locality = zipcode_and_locality[-1].strip()
-        localities.append(locality)
-        zipcodes.append(zipcode) 
-        print("Street name:" ,street_name)
-        print("Zipcode: ", zipcode)
-        print("Locality: ", locality)
-    else: 
-        zipcode_and_locality = address_row[0].text.strip().split('—')
-        zipcode = zipcode_and_locality[0].strip()
-        zipcodes.append(zipcode)
-        locality = zipcode_and_locality[-1].strip
-        localities.append(locality)
+    if address_rows is not None:
+        address_row = address_rows.find_all('span', {'class': 'classified__information--address-row'})
+        if address_row is not None:
+            if len(address_row)==2:
+                street_name = address_row[0].text.strip()
+                street_names.append(street_name)
+                zipcode_and_locality = address_row[1].text.strip().split('—')
+                zipcode = zipcode_and_locality[0].strip()
+                locality = zipcode_and_locality[-1].strip()
+                localities.append(locality)
+                zipcodes.append(zipcode) 
+                print("Street name:" ,street_name)
+                print("Zipcode: ", zipcode)
+                print("Locality: ", locality)
+            else: 
+                zipcode_and_locality = address_row[0].text.strip().split('—')
+                zipcode = zipcode_and_locality[0].strip()
+                zipcodes.append(zipcode)
+                locality = zipcode_and_locality[-1].strip
+                localities.append(locality)
+                street_names.append(None)
+                print("Zipcode: ", zipcode)
+                print("Locality: ", locality)
+                return
+        else:
+            street_names.append(None)
+            zipcodes.append(None)
+            localities.append(None)
+            street_names.append(None)
+    else:
         street_names.append(None)
-        print("Zipcode: ", zipcode)
-        print("Locality: ", locality)
-        return
-
+        zipcodes.append(None)
+        localities.append(None)
+        street_names.append(None)
 
 def get_the_rest(soup):
     
@@ -95,15 +119,15 @@ def get_the_rest(soup):
 def classified_table_data(classified_table_rows, target):
     for table_rows in classified_table_rows:
         table_row = table_rows.find('th', {'class': 'classified-table__header'})
-        if table_row != None:
+        if table_row is not None:
             if target in table_row.string:
                 answer = table_rows.find('td', {'class':'classified-table__data'})
-                if answer != None:
+                if answer is not None:
                     return answer.find(text=True).strip()
     return None
 
 counts = 0
-for i in range(1,):
+for i in range(1,11):
     url = f'https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&page={i}&orderBy=relevance'
     driver.get(url)
     mainsoup = BeautifulSoup(driver.page_source, 'lxml')
@@ -114,7 +138,7 @@ for i in range(1,):
             print(links['href'])
             driver.get(links['href'])
             soup = BeautifulSoup(driver.page_source, 'lxml')
-            get_property(links["href"])
+            get_property_and_primary_keys(links["href"])
             get_price(soup)
             get_address(soup)
             get_the_rest(soup)
@@ -126,10 +150,11 @@ for i in range(1,):
 
 df = pd.DataFrame()
 
+df["primary_keys"] = primary_keys
 df["property_types"] = property_types
 df["prices"] = prices
 df["zipcodes"] = zipcodes
-df["steet_names"] = street_names
+df["street_names"] = street_names
 df["localities"] = localities
 df["bedrooms"] = bedrooms
 df["bathrooms"] = bathrooms
